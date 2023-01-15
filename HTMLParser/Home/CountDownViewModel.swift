020f895cbdf77e7e58bb9e423dd7b0d8cb0d13e5
mika.urakawa <awakaru0122@gmail.com>
2023-01-15 15:45:27 +0900

    add: countdown until chrismas

--- HTMLParser/Home/CountDownViewModel.swift
+++ HTMLParser/Home/CountDownViewModel.swift
@@ -8,12 +8,49 @@
 import Foundation
 
 class CountDownViewModel: ObservableObject {
-    @Published var leftDays = "24"
-    @Published var leftHours = "11"
-    @Published var leftMins = "30"
-    @Published var leftSec = "22"
+    @Published var leftDays = "-"
+    @Published var leftHours = "-"
+    @Published var leftMins = "-"
+    @Published var leftSec = "-"
+    private var timer = Timer()
+    private var time = 5
 
     func startCount() {
+        // 1秒ごとに秒を減らしていく
+        // 毎秒12/25までの残り日数を計算してそれを反映する
+        // 前回のインスタンスが残っていた場合を想定して念の為削除
+        timer.invalidate()
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(self.countDownUntilChrismas),
+            userInfo: nil,
+            repeats: true
+        )
+    }
 
+    @objc private func countDownUntilChrismas() {
+        let startDate = "2023-12-25"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let formatedStartDate = dateFormatter.date(from: startDate)
+        let currentDate = Date()
+
+        if let leftDays = Calendar.current.dateComponents([.day], from: currentDate, to: formatedStartDate!).day {
+            self.leftDays = String(leftDays)
+        }
+        if let leftHours = Calendar.current.dateComponents([.hour], from: currentDate, to: formatedStartDate!).hour {
+            self.leftHours = String(leftHours % 24)
+        }
+        if let leftMins = Calendar.current.dateComponents([.minute], from: currentDate, to: formatedStartDate!).minute {
+            self.leftMins = String(leftMins % 60)
+        }
+        if let leftSec = Calendar.current.dateComponents([.second], from: currentDate, to: formatedStartDate!).second {
+            self.leftSec = String(leftSec % 60)
+        }
+
+        if self.leftDays == "0" && self.leftHours == "0" && self.leftMins == "0" && self.leftSec == "0" {
+            timer.invalidate()
+        }
     }
 }
